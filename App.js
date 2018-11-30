06260a0bcec070999d31d38a510ea575bb1d9cdf
Jessica <jchens@stanford.edu>
2018-11-30 02:43:38 -0800

    updated map

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Map from './App/Components/Map';
 
 export default class App extends React.Component {
   render() {
