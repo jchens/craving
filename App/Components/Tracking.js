e1b336f2e776280acbbcc6fe3bfa3241e73bbe99
Thomas Hsieh <thsieh42@gmail.com>
2018-11-30 08:57:07 -0800

    added buttoms and layout
    added buttoms and layout

--- App/Components/Tracking.js
+++ App/Components/Tracking.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types' //consider using this!
 import { StyleSheet, SafeAreaView, View, FlatList, Text, Linking, ActivityIndicator, TouchableOpacity, Image } from 'react-native'
-import { Metrics, Colors } from '../Themes'
 
 import {profilesList} from '../Themes/Profiles.js'
 
 export default class Tracking extends Component {
 
+
   render () {
 
-      console.log(profilesList)
-      return (
+    return (
         <View style={styles.container}>
             <Text style={styles.title}> Visted</Text>
 
@@ -21,25 +20,49 @@ export default class Tracking extends Component {
                 keyExtractor= {(item) => item.name}
                 renderItem={({item}) =>
 
+
                   <View style={styles.list_item}>
                     <Text style={styles.visit}> {item.visit} </Text>
 
                     <View style={styles.info}>
-                      <Image
-                        source={item.image}
-                        style={styles.image}
-                        resizeMode="contain"/>
-                      <View>
+                      <Image source={item.image} style={styles.image}/>
+                      <View style={styles.text}>
                         <Text style={styles.name}> {item.name} </Text>
                         <Text style={styles.cuisine}> {item.cuisine} </Text>
                         <Text style={styles.description}> {item.description} </Text>
-                      </View>
-                    </View>
 
+                        <View style={styles.bottom}>
+                          <View style={styles.bottom_button_left}>
+                            <TouchableOpacity>
+                              <Text style={styles.bottom_text_left}> Find on Map </Text>
+                            </TouchableOpacity>
+                          </View>
+
+
+                          <View style={styles.bottom_button_right}>
+                            <TouchableOpacity>
+                              <Text style={styles.bottom_text_right}> Check In </Text>
+                            </TouchableOpacity>
+                          </View>
+                        </View>
+
+
+                      </View>
+
+
+                      <View style={styles.button}>
+                        <TouchableOpacity>
+                          <Image
+                            style={styles.star}
+                            source={require('../Images/Icons/icons_star.png')}/>
+                        </TouchableOpacity>
+                      </View>
+
+
+                    </View>
                   </View>
                 }
               />
-
             </View>
 
         </View>
@@ -56,14 +79,10 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'stretch',
   },
-
   title: {
     flex: 0.1,
-    font-size: 24;
-    fontFamily: 'PingFangHK-Medium',
-    color: Colors.gray4,
-  },
 
+  },
   list_container: {
     flex: 1,
     backgroundColor: 'blue',
@@ -72,45 +91,89 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
-
-
   list_item: {
-    flex: 0.5,
-    backgroundColor: 'blue',
-    paddingBottom: 20,
+  flex: 0.5,
+  backgroundColor: 'blue',
+  paddingBottom: 20,
   },
-
   visit: {
-    font-size: 14,
-    color: Colors.blue,
-    fontFamily: 'PingFangHK-Light',
+    color: '#0496FF',
   },
-
-  // view to contain text
   info: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     alignItems: 'flex-start',
     padding: 20,
-
   },
-
   image: {
     height: 100,
     width: 100,
   },
+  text: {
+    width: 200,
+    marginLeft: 30,
+    marginRight: 30,
+
+  },
   name: {
-    font-size: 24;
-    color: Colors.gray1,
+    color: '#333333',
 
   },
   cuisine: {
-    font-size: 14;
-    color: Colors.gray4,
+    color: '#BDBDBD',
   },
   description: {
-    font-size: 14;
-    color: Colors.gray3,
+    color: '#828282',
+    flexWrap: 'wrap',
+    backgroundColor: 'yellow'
   },
+  button: {
+    backgroundColor: '#F2F2F2',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 80,
+    width: 80,
+    borderRadius: 40
+
+  },
+  star: {
+    backgroundColor: '#F2F2F2',
+    height: 50,
+    width: 50,
+
+  },
+  bottom: {
+    flexDirection: 'row',
+  },
+  bottom_button_left: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    width: 140,
+    borderRadius: 40,
+    borderWidth: 2,
+    borderColor: '#FF4D00',
+  },
+  bottom_button_right: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    width: 140,
+    borderRadius: 40,
+    borderWidth: 2,
+    borderColor: '#0496FF',
+  },
+  bottom_text_left: {
+    color: '#FF4D00',
+
+  },
+  bottom_text_right: {
+    color: '#0496FF',
+
+  }
+
+
 
 });
