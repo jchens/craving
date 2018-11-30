2e0ef734b2674f5a440192c3eb26f4ec2e73779d
Jessica <jchens@stanford.edu>
2018-11-30 01:18:21 -0800

    Update Colors.js

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -1,36 +1,17 @@
-/*
-*
-* Assignment 3
-* Starter Files
-*
-* CS47SI
-* Jan, 2017
-*/
-
 const colors = {
   background: '#1F0808',
   clear: 'rgba(0,0,0,0)',
-  facebook: '#3b5998',
-  transparent: 'rgba(0,0,0,0)',
-  silver: '#F7F7F7',
-  steel: '#CCCCCC',
-  error: 'rgba(200, 0, 0, 0.8)',
-  ricePaper: 'rgba(255,255,255, 0.75)',
-  frost: '#D8D8D8',
-  cloud: 'rgba(200,200,200, 0.35)',
-  windowTint: 'rgba(0, 0, 0, 0.4)',
-  panther: '#161616',
-  charcoal: '#595959',
-  coal: '#2d2d2d',
-  bloodOrange: '#fb5f26',
-  snow: 'white',
-  ember: 'rgba(164, 0, 48, 0.5)',
-  fire: '#e73536',
-  drawer: 'rgba(30, 30, 29, 0.95)',
-  eggplant: '#251a34',
-  border: '#483F53',
-  banner: '#5F3E63',
-  text: '#E0D7E5'
+  white: '#FFFFFF',
+  orange: '#FF4D00',
+  yellow: '#FFD046',
+  blue: '#0496FF',
+  purple: '#241E4E',
+  gray1: '#333333',
+  gray2: '#4F4F4F',
+  gray3: '#828282',
+  gray4: '#BDBDBD',
+  gray5: '#E0E0E0',
+  gray6: '#F2F2F2',
 }
 
 export default colors
