53c4ee3902229a111f118e63815a62ff86b2455a
Jessica <jchens@stanford.edu>
2018-11-30 04:27:53 -0800

    updated map

--- App/Components/PriceMarker.js
+++ App/Components/PriceMarker.js
@@ -1,77 +1,106 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 import {
-  StyleSheet,
-  View,
   Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  Image,
 } from 'react-native';
 
-const propTypes = {
-  amount: PropTypes.number.isRequired,
-  fontSize: PropTypes.number,
-};
+import MapView, { Marker, Callout, ProviderPropType } from 'react-native-maps';
+import flagImg from '../Images/Icons/icons_pin_orange.png';
 
-const defaultProps = {
-  fontSize: 13,
-};
+const { width, height } = Dimensions.get('window');
 
-export default class PriceMarker extends Component {
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+
+export default class TruckMap extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+    };
+  }
+
   render() {
-    const { fontSize, amount } = this.props;
     return (
       <View style={styles.container}>
-        <View style={styles.bubble}>
-          <Text style={styles.dollar}>$</Text>
-          <Text style={[styles.amount, { fontSize }]}>{amount}</Text>
+        <MapView
+          provider={this.props.provider}
+          style={styles.map}
+          initialRegion={this.state.region}
+          onPress={this.onMapPress}
+          loadingEnabled
+          loadingIndicatorColor="#666666"
+          loadingBackgroundColor="#eeeeee"
+        >
+          <Marker
+            coordinate={{
+              latitude: LATITUDE + SPACE,
+              longitude: LONGITUDE + SPACE,
+            }}
+            anchor={{ x: 0.5, y: 1 }}
+          >
+          <Image source={require('../Images/Icons/icons_pin_orange.png')} style={{ width: 40, height: 40 }} />
+          </Marker>
+
+          <Marker
+            coordinate={{
+              latitude: LATITUDE - SPACE,
+              longitude: LONGITUDE - SPACE,
+            }}
+            anchor={{ x: 0.5, y: 1 }}
+          >
+            <Callout>
+              <View>
+                <Text>This is a plain view</Text>
+              </View>
+            </Callout>
+          </Marker>
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <View style={styles.bubble}>
+            <Text>Map with Loading</Text>
+          </View>
         </View>
-        <View style={styles.arrowBorder} />
-        <View style={styles.arrow} />
       </View>
     );
   }
 }
 
-PriceMarker.propTypes = propTypes;
-PriceMarker.defaultProps = defaultProps;
+TruckMap.propTypes = {
+  provider: ProviderPropType,
+};
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
-    alignSelf: 'flex-start',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
   bubble: {
-    flex: 0,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  buttonContainer: {
     flexDirection: 'row',
-    alignSelf: 'flex-start',
-    backgroundColor: '#FF5A5F',
-    padding: 2,
-    borderRadius: 3,
-    borderColor: '#D23F44',
-    borderWidth: 0.5,
-  },
-  dollar: {
-    color: '#FFFFFF',
-    fontSize: 10,
-  },
-  amount: {
-    color: '#FFFFFF',
-    fontSize: 13,
-  },
-  arrow: {
+    marginVertical: 20,
     backgroundColor: 'transparent',
-    borderWidth: 4,
-    borderColor: 'transparent',
-    borderTopColor: '#FF5A5F',
-    alignSelf: 'center',
-    marginTop: -9,
-  },
-  arrowBorder: {
-    backgroundColor: 'transparent',
-    borderWidth: 4,
-    borderColor: 'transparent',
-    borderTopColor: '#D23F44',
-    alignSelf: 'center',
-    marginTop: -0.5,
   },
 });
--- App/Components/TruckMap.js
+++ App/Components/TruckMap.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 
+import React from 'react';
 import {
   StyleSheet,
   View,
-  Text,
   Dimensions,
-  ScrollView,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
-// eslint-disable-next-line max-len
-import MapView, { PROVIDER_GOOGLE, Marker, ProviderPropType, Polygon, Polyline, Callout } from 'react-native-maps';
-import PriceMarker from './PriceMarker';
+
+import MapView, { Marker } from 'react-native-maps';
 
 const { width, height } = Dimensions.get('window');
 
@@ -19,208 +17,99 @@ const LATITUDE = 37.78825;
 const LONGITUDE = -122.4324;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-let id = 0;
+const SPACE = 0.01;
 
-class Event extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return this.props.event.id !== nextProps.event.id;
-  }
-
-  render() {
-    const { event } = this.props;
-    return (
-      <View style={styles.event}>
-        <Text style={styles.eventName}>{event.name}</Text>
-        <Text style={styles.eventData}>{JSON.stringify(event.data, null, 2)}</Text>
-      </View>
-    );
-  }
+function createMarker(modifier = 1) {
+  return {
+    latitude: LATITUDE - (SPACE * modifier),
+    longitude: LONGITUDE - (SPACE * modifier),
+  };
 }
 
-Event.propTypes = {
-  event: PropTypes.object,
-};
+const MARKERS = [
+  createMarker(),
+  createMarker(2),
+  createMarker(3),
+  createMarker(4),
+];
 
+const DEFAULT_PADDING = { top: 40, right: 40, bottom: 40, left: 40 };
 
-// eslint-disable-next-line react/no-multi-comp
-export default class TruckMap extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
-      events: [],
-    };
+class FitToCoordinates extends React.Component {
+  fitPadding() {
+    this.map.fitToCoordinates([MARKERS[2], MARKERS[3]], {
+      edgePadding: { top: 100, right: 100, bottom: 100, left: 100 },
+      animated: true,
+    });
   }
 
-  makeEvent(e, name) {
-    return {
-      id: id++,
-      name,
-      data: e.nativeEvent ? e.nativeEvent : e,
-    };
+  fitBottomTwoMarkers() {
+    this.map.fitToCoordinates([MARKERS[2], MARKERS[3]], {
+      edgePadding: DEFAULT_PADDING,
+      animated: true,
+    });
   }
 
-  recordEvent(name) {
-    return e => {
-      if (e.persist) {
-        e.persist();  // Avoids warnings relating to https://fb.me/react-event-pooling
-      }
-      this.setState(prevState => ({
-        events: [
-          this.makeEvent(e, name),
-          ...prevState.events.slice(0, 10),
-        ],
-      }));
-    };
+  fitAllMarkers() {
+    this.map.fitToCoordinates(MARKERS, {
+      edgePadding: DEFAULT_PADDING,
+      animated: true,
+    });
   }
 
   render() {
-    // Events that are dependent on
-    let googleProviderProps = {};
-    if (this.props.provider === PROVIDER_GOOGLE) {
-      googleProviderProps = {
-        onUserLocationChange: this.recordEvent('Map::onUserLocationChange'),
-      };
-    }
-
     return (
       <View style={styles.container}>
         <MapView
-          provider={this.props.provider}
+          ref={ref => { this.map = ref; }}
           style={styles.map}
-          initialRegion={this.state.region}
-          showsUserLocation
-          showsMyLocationButton
-          onRegionChange={this.recordEvent('Map::onRegionChange')}
-          onRegionChangeComplete={this.recordEvent('Map::onRegionChangeComplete')}
-          onPress={this.recordEvent('Map::onPress')}
-          onPanDrag={this.recordEvent('Map::onPanDrag')}
-          onLongPress={this.recordEvent('Map::onLongPress')}
-          onMarkerPress={this.recordEvent('Map::onMarkerPress')}
-          onMarkerSelect={this.recordEvent('Map::onMarkerSelect')}
-          onMarkerDeselect={this.recordEvent('Map::onMarkerDeselect')}
-          onCalloutPress={this.recordEvent('Map::onCalloutPress')}
-          {...googleProviderProps}
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
         >
-          <Marker
-            coordinate={{
-              latitude: LATITUDE + (LATITUDE_DELTA / 2),
-              longitude: LONGITUDE + (LONGITUDE_DELTA / 2),
-            }}
-          />
-          <Marker
-            coordinate={{
-              latitude: LATITUDE - (LATITUDE_DELTA / 2),
-              longitude: LONGITUDE - (LONGITUDE_DELTA / 2),
-            }}
-          />
-          <Marker
-            title="This is a title"
-            description="This is a description"
-            coordinate={this.state.region}
-            onPress={this.recordEvent('Marker::onPress')}
-            onSelect={this.recordEvent('Marker::onSelect')}
-            onDeselect={this.recordEvent('Marker::onDeselect')}
-            onCalloutPress={this.recordEvent('Marker::onCalloutPress')}
-          >
-            <PriceMarker amount={99} />
-            <Callout
-              style={styles.callout}
-              onPress={this.recordEvent('Callout::onPress')}
-            >
-              <View>
-                <Text>Well hello there...</Text>
-              </View>
-            </Callout>
-          </Marker>
-          <Polygon
-            fillColor={'rgba(255,0,0,0.3)'}
-            onPress={this.recordEvent('Polygon::onPress')}
-            tappable
-            coordinates={[{
-              latitude: LATITUDE + (LATITUDE_DELTA / 5),
-              longitude: LONGITUDE + (LONGITUDE_DELTA / 4),
-            }, {
-              latitude: LATITUDE + (LATITUDE_DELTA / 3),
-              longitude: LONGITUDE + (LONGITUDE_DELTA / 4),
-            }, {
-              latitude: LATITUDE + (LATITUDE_DELTA / 4),
-              longitude: LONGITUDE + (LONGITUDE_DELTA / 2),
-            }]}
-          />
-          <Polyline
-            strokeColor={'rgba(255,0,0,1)'}
-            onPress={this.recordEvent('Polyline::onPress')}
-            tappable
-            coordinates={[{
-              latitude: LATITUDE + (LATITUDE_DELTA / 5),
-              longitude: LONGITUDE - (LONGITUDE_DELTA / 4),
-            }, {
-              latitude: LATITUDE + (LATITUDE_DELTA / 3),
-              longitude: LONGITUDE - (LONGITUDE_DELTA / 4),
-            }, {
-              latitude: LATITUDE + (LATITUDE_DELTA / 4),
-              longitude: LONGITUDE - (LONGITUDE_DELTA / 2),
-            }]}
-          />
+          {MARKERS.map((marker, i) => (
+            <Marker
+              key={i}
+              coordinate={marker}
+            />
+          ))}
         </MapView>
-        <View style={styles.eventList}>
-          <ScrollView>
-            {this.state.events.map(event => <Event key={event.id} event={event} />)}
-          </ScrollView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.fitPadding()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Fit Bottom Two Markers with Padding</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.fitBottomTwoMarkers()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Fit Bottom Two Markers</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.fitAllMarkers()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Fit All Markers</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
   }
 }
 
-TruckMap.propTypes = {
-  provider: ProviderPropType,
-};
-
 const styles = StyleSheet.create({
-  callout: {
-    width: 60,
-  },
   container: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  event: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    padding: 8,
-  },
-  eventData: {
-    fontSize: 10,
-    fontFamily: 'courier',
-    color: '#555',
-  },
-  eventName: {
-    fontSize: 13,
-    fontWeight: 'bold',
-    color: '#222',
-  },
-  eventList: {
-    position: 'absolute',
-    top: height / 2,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
   map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: height / 2,
+    ...StyleSheet.absoluteFillObject,
   },
   bubble: {
     backgroundColor: 'rgba(255,255,255,0.7)',
@@ -228,19 +117,17 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     borderRadius: 20,
   },
-  latlng: {
-    width: 200,
-    alignItems: 'stretch',
-  },
   button: {
-    width: 80,
+    marginTop: 12,
     paddingHorizontal: 12,
     alignItems: 'center',
     marginHorizontal: 10,
   },
   buttonContainer: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     marginVertical: 20,
     backgroundColor: 'transparent',
   },
 });
+
+export default FitToCoordinates;
