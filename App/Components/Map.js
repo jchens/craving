0862c6fcc7694b6f17165a055b22687ae4d61030
Jessica <jchens@stanford.edu>
2018-11-30 01:51:32 -0800

    added Map

---
+++ App/Components/Map.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet,
+  View,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  Dimensions,
+  ScrollView} from 'react-native'
+import { material } from 'react-native-typography'
+import { FontAwesome } from '@expo/vector-icons';
+import { Metrics, Colors } from '../Themes'
+import { MapView, Marker } from 'react-native-maps';
+
+
+
+export default class Search extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Search for News' };
+  }
+
+
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+
+      <MapView
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+      >
+        {this.state.markers.map(marker => (
+          <Marker
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+
+  search: {
+    flexDirection: 'row',
+    backgroundColor: Colors.snow,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 0,
+  },
+
+  search_bar: {
+    height: 50,
+    flex: 0.9,
+    flexDirection: 'row',
+    backgroundColor: Colors.silver,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+  },
+
+  input: {
+    width: Dimensions.get('window').width - 100,
+    color: Colors.charcoal,
+  },
+
+  search_icon: {
+    height: 30,
+    aspectRatio: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
