a872dd003889f8b41a6f664c1dcce93c348af88a
Thomas Hsieh <thsieh42@gmail.com>
2018-12-03 22:44:35 -0800

    update rewards
    update rewards

--- App/Components/Rewards.js
+++ App/Components/Rewards.js
@@ -14,22 +14,55 @@ export default class Rewards extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.reward}>
-          <Text> Rewards</Text>
+          <Text style={styles.title}> Rewards</Text>
         </View>
         <View style={styles.craving}>
-          <Text> TODAYS CRAVING </Text>
+          <Text style={styles.title}> TODAYS CRAVING </Text>
+
           <View style={styles.goals}>
             <View style={styles.card}>
-              <Text> hey </Text>
+
+              <View style={styles.symbol}>
+                <View style={styles.circle}>
+                  <Image
+                    style={styles.star}
+                    source={Images.star}/>
+                </View>
+                <Text style={styles.points}> + 10 PTS </Text>
+              </View>
+
+              <Text style={styles.text}> FIND A FOOD TRUCK NEARBY </Text>
             </View>
 
             <View style={styles.card}>
-              <Text> test </Text>
+
+              <View style={styles.symbol}>
+                <View style={styles.circle}>
+                  <Image
+                    style={styles.star}
+                    source={Images.star}/>
+                </View>
+                <Text style={styles.points}> + 25 PTS </Text>
+              </View>
+
+              <Text style={styles.text}> VISIT A FOOD TRUCK </Text>
+
+
+            </View>
+            <View style={styles.card}>
+
+              <View style={styles.symbol}>
+                <View style={styles.circle}>
+                  <Image
+                    style={styles.star}
+                    source={Images.star}/>
+                </View>
+                <Text style={styles.points}> + 50 PTS </Text>
+              </View>
+
+              <Text style={styles.text}> LEAVE A REVIEW </Text>
             </View>
 
-            <View style={styles.card}>
-              <Text> hewwwy </Text>
-            </View>
 
           </View>
         </View>
@@ -45,6 +78,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  title: {
+    marginLeft: 25,
+  },
   reward: {
     flex: 2,
     backgroundColor: 'green',
@@ -52,6 +88,7 @@ const styles = StyleSheet.create({
   craving: {
     flex: 8,
     backgroundColor: 'blue',
+    justifyContent: 'space-between'
   },
   goals: {
     flex: 1,
@@ -61,9 +98,40 @@ const styles = StyleSheet.create({
   },
   card: {
     backgroundColor: 'red',
-    width: 300,
-    height: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 15,
+    width: 330,
+    height: 117,
     margin: 20,
+  },
+  symbol: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  circle: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+  },
+  star: {
+    backgroundColor: 'green',
+    height: 30,
+    width: 30,
+  },
+  points: {
+    marginTop: 5,
+    color: 'white',
 
-  }
+  },
+  text: {
+    width: 125,
+    marginBottom: 15,
+    marginLeft: 60,
+    color: 'white',
+  },
 });
