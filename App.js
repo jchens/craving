549b6b6fcaab71a317658c7fcf6760ac2966d2b6
Thomas Hsieh <thsieh42@gmail.com>
2018-12-05 05:04:01 -0800

    adding fonts woooo finally

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
-import Tracking from './App/Components/Tracking';
-import HomeMap from './App/Components/HomeMap';
+//import Tracking from './App/Components/Tracking';
+//import HomeMap from './App/Components/HomeMap';
 //import TruckMap from './App/Components/TruckMap';
 //import Rewards from './App/Components/Rewards';
+import { Font } from 'expo';
+
 
 
 import Icon from '@expo/vector-icons/MaterialCommunityIcons'
 
 
 export default class App extends React.Component {
+
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'lato-regular': require('./assets/fonts/Lato-Regular.ttf'),
+      'lato-bold': require('./assets/fonts/Lato-Bold.ttf'),
+      'lato-light': require('./assets/fonts/Lato-Light.ttf'),
+
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <HomeMap />
+        {
+          this.state.fontLoaded ? (
+            <Text style={{ fontFamily: 'lato-bold', fontSize: 20 }}>
+              Hello, world!
+            </Text>
+          ) : null
+        }
+        <Text style={{ fontSize: 20 }}> Hello, world! is there a difference lmao</Text>
       </View>
     );
   }
