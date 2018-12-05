230e20e97747b1b89ebe46e882db17fb8815805d
Thomas Hsieh <thsieh42@gmail.com>
2018-12-04 22:37:15 -0800

    updating rewards
    rewards

--- App/Components/Rewards.js
+++ App/Components/Rewards.js
@@ -1,24 +1,83 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types' //consider using this!
-import { StyleSheet, SafeAreaView, View, FlatList, Text, Linking, ActivityIndicator, TouchableOpacity, Image } from 'react-native'
+import { StyleSheet, SafeAreaView, View, FlatList, Text, Linking, ActivityIndicator, TouchableOpacity, Image, Dimensions, Button, Alert} from 'react-native'
 import { Metrics, Colors, Images } from '../Themes'
 import {profilesList} from '../Themes/Profiles.js'
 
+import ProgressBarAnimated from 'react-native-progress-bar-animated';
+
+
+
 export default class Rewards extends Component {
 
+  state = {
+    progress: 20,
+    progressWithOnComplete: 0,
+    progressCustomized: 0,
+  }
+
+  increase = (key, value) => {
+    this.setState({
+      [key]: this.state[key] + value,
+    });
+  }
 
 
 
   render () {
 
+    const barWidth = Dimensions.get('screen').width - 85;
+    const progressCustomStyles = {
+      backgroundColor: '#FF4D00',
+      borderRadius: 0,
+      borderColor: '#FF4D00',
+    };
+
     return (
       <View style={styles.container}>
+
         <View style={styles.reward}>
           <Text style={styles.titleRewards}> Rewards</Text>
-        </View>
+
+          <View style={styles.progress}>
+
+            <View style={styles.checkpoints}>
+              <View style={styles.outer1}>
+                <View style={styles.inner}>
+                </View>
+              </View>
+              <View style={styles.outer2}>
+                <View style={styles.inner}>
+                </View>
+              </View>
+            <View>
+
+              <View style={styles.bar}>
+
+
+                <ProgressBarAnimated
+                  {...progressCustomStyles}
+                  width={barWidth}
+                  value={this.state.progressWithOnComplete}
+                  onComplete={() => {
+                    Alert.alert('Hey!', 'onComplete event fired!');
+                  }}
+                />
+              </View>
+              <View style={styles.buttonContainer}>
+                <View style={styles.buttonInner}>
+                  <Button
+                    title="Increase 25%"
+                    onPress={this.increase.bind(this, 'progressWithOnComplete', 25)}
+                  />
+                </View>
+              </View>
+            </View>
+          </View>
+
+
         <View style={styles.craving}>
           <Text style={styles.titleCraving}> TODAYS CRAVING </Text>
-
           <View style={styles.goals}>
             <View style={styles.card1}>
 
@@ -42,7 +101,7 @@ export default class Rewards extends Component {
                     style={styles.star}
                     source={Images.star}/>
                 </View>
-                <Text style={styles.points}> + 25 PTS </Text>
+                <Text style={styles.points}> + 10 PTS </Text>
               </View>
 
               <Text style={styles.text}> VISIT A FOOD TRUCK </Text>
@@ -57,7 +116,7 @@ export default class Rewards extends Component {
                     style={styles.star}
                     source={Images.star}/>
                 </View>
-                <Text style={styles.points}> + 50 PTS </Text>
+                <Text style={styles.points}> + 10 PTS </Text>
               </View>
 
               <Text style={styles.text}> LEAVE A REVIEW </Text>
@@ -88,9 +147,59 @@ const styles = StyleSheet.create({
   reward: {
     flex: 2,
   },
+  progress: {
+    alignItems: 'center',
+  },
+  outer1: {
+    backgroundColor: '#FF4D00',
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inner: {
+    backgroundColor: 'white',
+    height: 16,
+    width: 16,
+    borderRadius: 8,
+  },
+  outer2: {
+    backgroundColor: '#FF4D00',
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  outer3: {
+    backgroundColor: '#FF4D00',
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 300,
+    marginTop: 2.5,
+  },
+  outer4: {
+    backgroundColor: '#FF4D00',
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 300,
+    marginTop: 2.5,
+  },
+  bar: {
+    position: 'absolute',
+    paddingTop: 10,
+
+  },
   craving: {
     flex: 8,
-    justifyContent: 'space-between'
   },
   goals: {
     flex: 1,
@@ -152,5 +261,11 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     marginLeft: 60,
     color: 'white',
+  },
+  label: {
+    color: '#999',
+    fontSize: 14,
+    fontWeight: '500',
+    marginBottom: 10,
   },
 });
