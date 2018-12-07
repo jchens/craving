492c5c6b72cd9a518955538de36ab8f42f8095fb
jenntao <jenntao@stanford.edu>
2018-12-07 00:06:28 -0800

    fix merge problems

--- App.js
+++ App.js
@@ -1,115 +1,29 @@
 import React, { Component } from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator, TabBarBottom, createAppContainer, createStackNavigator } from 'react-navigation';
-import HomeMap from './App/Components/HomeMap';
-//import Rewards from './App/Components/Rewards';
-import Visited from './App/Components/Visited';
-import Followed from './App/Components/Followed';
-import Profile from './App/Components/Profile';
-import ProfileA from './App/Components/ProfileA';
-import { Feather, MaterialIcons, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
-import Onboarding from 'react-native-onboarding-swiper';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import Onboard from './App/Components/Onboard';
+import NavBar from './App/Components/NavBar';
 
-import { Font } from 'expo';
 import { Metrics, Colors, Images } from './App/Themes';
 
-import Icon from '@expo/vector-icons/MaterialCommunityIcons';
+/* 'App' now gets navigation from onboarding to the rest of the app to work. */
 
 const StackNav = createStackNavigator(
   {
-    HomeMap: {
-      screen: HomeMap,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <Feather
-            name='map-pin'
-            color={tintColor}
-            size={20}
-          />
-        ),
-        tabBarLabel: 'Map',
-      },
-    },
-    //Rewards: Rewards,
-
-    Rewards: {
-      screen: ProfileA,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <Feather
-            name='award'
-            color={tintColor}
-            size={20}
-          />
-        ),
-      },
-    },
-
-    Followed: {
-      screen: Followed,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <Feather
-            name='star'
-            color={tintColor}
-            size={20}
-          />
-        ),
-        tabBarLabel: 'Followed',
-      },
-    },
-
-    Visited: {
-      screen: Visited,
-      navigationOptions: {
-        tabBarIcon: ({tintColor}) => (
-          <Feather
-            name='clock'
-            color={tintColor}
-            size={20}
-          />
-        ),
-        tabBarLabel: 'Followed',
-      },
-    },
-
+    // these names suck lol I am bad with names :(
+    Onboarding: Onboard,
+    Main: NavBar,
   },
   {
-    initialRouteName: 'Rewards',
-    order: ['HomeMap', 'Rewards', 'Followed', 'Visited'],
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
-    tabBarOptions: {
-      showLabel: false,
-      activeTintColor: Colors.orange,
-      inactiveTintColor: Colors.gray1,
-      //activeBackgroundColor: Colors.gray6,
-      tabStyle: {
-        //paddingBottom: Metrics.padSmall, // note: idk if this is needed, but the Android bar is in the way
-        borderTopWidth: 1,
-        borderColor: Colors.gray5,
-      },
-      style: {
-        flexDirection: 'row',
-        height: Metrics.nav,
-        backgroundColor: Colors.gray7,
-        alignItems: 'center',
-        justifyContent: 'center',
-
-        // shadowColor: Colors.black,
-        // shadowOpacity: Metrics.glow / 10,
-        // shadowRadius: 8,
-        // shadowOffset: {width: 0, height: -5},
-
-        zIndex: 1,
-      },
-    },
-    animationEnabled: false, // idk what this does lol
-    swipeEnabled: true, // I don't think it actually supports swipes lol
+    /* These options hide the stack navigator at the top. */
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false
+    }
   }
 );
 
-export default createAppContainer(TabNavigator);
+export default createAppContainer(StackNav);
 
 const styles = StyleSheet.create({
   container: {
