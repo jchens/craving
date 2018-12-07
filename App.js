1fd1126e1316b6e7b080084ea9f800f93289b998
jenntao <jenntao@stanford.edu>
2018-12-06 23:32:53 -0800

    implement onboarding with placeholder content

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator, TabBarBottom, createAppContainer} from 'react-navigation';
+import { createBottomTabNavigator, TabBarBottom, createAppContainer, createStackNavigator } from 'react-navigation';
 import HomeMap from './App/Components/HomeMap';
 //import Rewards from './App/Components/Rewards';
 import Visited from './App/Components/Visited';
 import Followed from './App/Components/Followed';
 import Profile from './App/Components/Profile';
 import NavBar from './App/Components/NavBar';
+import Onboard from './App/Components/Onboard';
 import { Feather, MaterialIcons, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 import Onboarding from 'react-native-onboarding-swiper';
 
@@ -15,11 +16,20 @@ import { Metrics, Colors, Images } from './App/Themes';
 
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 
-/* Placeholder */
-export default class App extends Component {
-  render() {
-    return (
-      <NavBar />
-    );
+const StackNav = createStackNavigator(
+  {
+    // these names suck lol I am bad with names :(
+    Onboarding: Onboard,
+    Main: NavBar,
+  },
+  {
+    /* These options hide the stack navigator at the top. */
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false
+    }
   }
-}
+);
+
+/* Main app gets navigation from onboarding to the rest of the app to work. */
+export default createAppContainer(StackNav);
---
+++ App/Components/Onboard.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
+import { Metrics, Colors, Images } from '../Themes';
+
+export default class Onboard extends Component {
+  doneCallback = () => {
+    /* Hitting the done button navigates to the rest of the app. */
+    this.props.navigation.navigate('Main');
+  }
+
+  skipCallback = () => {
+    /* Hitting the skip button navigates to the rest of the app. */
+    this.props.navigation.navigate('Main');
+  }
+
+  render() {
+    return (
+      <Onboarding
+        onDone={() => this.doneCallback()}
+        onSkip={() => this.skipCallback()}
+        pages={[
+          {
+            backgroundColor: Colors.yellow,
+            image: <Image source={require('../Images/Onboarding/placeholder.png')} />,
+            title: 'Onboarding',
+            subtitle: 'Done with React Native Onboarding Swiper',
+          },
+          {
+            backgroundColor: Colors.blue,
+            image: <Image source={require('../Images/Onboarding/placeholder2.png')} />,
+            title: 'Onboarding',
+            subtitle: 'Done with React Native Onboarding Swiper',
+          },
+          {
+            backgroundColor: Colors.orange,
+            image: <Image source={require('../Images/Onboarding/placeholder3.png')} />,
+            title: 'Onboarding',
+            subtitle: 'Done with React Native Onboarding Swiper',
+          }
+        ]}
+      />
+    );
+  }
+}
