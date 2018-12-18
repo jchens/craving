998a4e44abe6cf245255944476efcb91acce4bee
jenntao <jenntao@stanford.edu>
2018-12-17 23:59:39 -0800

    add firebase to app

--- App.js
+++ App.js
@@ -3,8 +3,18 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import Onboard from './App/Components/Onboard';
 import NavBar from './App/Components/NavBar';
+import * as firebase from 'firebase';
 
 import { Metrics, Colors, Images } from './App/Themes';
+
+/* Initialize Firebase */
+const firebaseConfig = {
+  apiKey: "AIzaSyAra3IIAi3Xdf9IFrxY9y4AQUq43yyR2ZM",
+  authDomain: "craving-48bc5.firebaseapp.com",
+  databaseURL: "https://FoodTrucks.firebaseio.com",
+  storageBucket: "craving-48bc5.appspot.com"
+};
+firebase.initializeApp(firebaseConfig);
 
 /* 'App' now gets navigation from onboarding to the rest of the app to work. */
 
